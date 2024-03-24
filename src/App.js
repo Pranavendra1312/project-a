b5305082d7b2ac6ac243abc90e97d9fc77f8d0b6
Pranav Konda <Pranav.Konda@happy.com>
2024-03-24 22:47:40 +0530

    Added aggregate and team attendance

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
+import { useState } from 'react';
 import './App.css';
 import BarChart from './components/Barchart';
 import TeamAttendanceChart from './components/TeamAttendanceChart';
 
 function App() {
+  const [isBarChartDisplayed, setIsBarChartDisplayed] = useState(true); // State to track which chart is displayed
+
+  
+  const handleBarChartButtonClick = () => {
+    setIsBarChartDisplayed(true); 
+  };
+
+  
+  const handleTeamAttendanceButtonClick = () => {
+    setIsBarChartDisplayed(false); 
+  };
+
   return (
     <div className="App">
-      <p>Hello</p>
-      <BarChart/>
-      <TeamAttendanceChart/>
+      
+      <button onClick={handleBarChartButtonClick} style={{ fontWeight: isBarChartDisplayed ? 'bold' : 'normal' }}>Aggregate Attendance</button>
+      <button onClick={handleTeamAttendanceButtonClick} style={{ fontWeight: !isBarChartDisplayed ? 'bold' : 'normal' }}>Team Attendance</button>
+      {isBarChartDisplayed ? <BarChart /> : <TeamAttendanceChart />}
+      {/* <BarChart/>
+      <TeamAttendanceChart/> */}
     </div>
   );
 }
